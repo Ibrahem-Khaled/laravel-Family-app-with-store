2d84c0517ce82fc83dcd66d3b369d3d76216b992
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-04 14:34:57 +0200

    add uuid in modal

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected static function booted()
+    {
+        static::creating(function ($user) {
+            $user->uuid = Str::uuid(); // استخدام UUID حقيقي
+        });
+    }
     protected $fillable = [
         'uuid',
         'api_token',
@@ -33,6 +40,7 @@ class User extends Authenticatable
     ];
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'uuid' => 'string',
     ];
 
     public function subscriptions()
